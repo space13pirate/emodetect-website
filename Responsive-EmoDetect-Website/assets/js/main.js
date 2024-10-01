3488a3ec4416145afd074885d7077e71ee456a61
Anastasia <north13anastasia@gmail.com>
2024-10-01 19:47:20 +0300

    Continue website development

--- Responsive-EmoDetect-Website/assets/js/main.js
+++ Responsive-EmoDetect-Website/assets/js/main.js
@@ -27,26 +27,169 @@ const linkAction = () =>{
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*=============== SWIPER PROJECTS ===============*/
+/*=============== SWIPER DEMO ===============*/
+let swiperDemo = new Swiper(".demo__container", {
+    loop: true,
+    spaceBetween:24,
 
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
 
-/*=============== SWIPER TESTIMONIAL ===============*/
+    // mousewheel: true,
+    // keyboard: true,
 
+    breakpoints: {
+        1200: {
+            slidesPerView: 2,
+            spaceBetween: -56,
+        },
+    },
+});
+
+/*=============== SWIPER IMPLEMENTATION ===============*/
+let swiperImplementatio = new Swiper(".implementation__container", {
+    grabCursor: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactName = document.getElementById('contact-name'),
+      contactEmail = document.getElementById('contact-email'),
+      contactFeedback = document.getElementById('contact-feedback'),
+      contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // Check if the field has a value
+    if(contactName.value === '' || contactEmail.value === '' || contactFeedback.value === ''){
+        // Add and remove color
+        contactMessage.classList.remove('color-blue')
+        contactMessage.classList.add('color-red')
+
+        // Show message	
+        contactMessage.textContent = 'Пожалуйста, заполните все поля... 📩'
+    }else{
+        // serviceID - templateID - #form - publicKey
+        emailjs.sendForm('service_5yyiwz4','template_08i5zxa','#contact-form','einvUhwqBX5FLUnG8')
+            .then(() => {
+                // Show message and add color
+                contactMessage.classList.add('color-blue')
+                contactMessage.textContent = 'Сообщение отправлено ✅'
+
+                // Remove message after five seconds
+                setTimeout(() =>{
+                    contactMessage.textContent = ''
+                }, 5000)
+            }, (error) =>{
+                alert('Упс! Что-то пошло не так...', error)
+            })
+
+        // To clear the input field
+        contactName.value = ''
+        contactEmail.value = ''
+        contactFeedback.value = ''
+    }
+}
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
 
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-clear-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-clear-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true /* Animation repeat*/
+})
 
+sr.reveal(`.home__data, .demo__container, .implementation__container, .footer__container`)
+sr.reveal(`.home__info div`, {delay: 600, origin: 'bottom', interval: 100})
+
+sr.reveal(`.features__content, .achievements__card`, {interval: 100})
+
+sr.reveal(`.toolset__content:nth-child(1)`, {origin: 'left'})
+sr.reveal(`.toolset__content:nth-child(3)`, {origin: 'left'})
+sr.reveal(`.toolset__content:nth-child(2)`, {origin: 'right'})
+sr.reveal(`.toolset__content:nth-child(4)`, {origin: 'right'})
+
+sr.reveal(`.contact__content:nth-child(1)`, {origin: 'left'})
+sr.reveal(`.contact__content:nth-child(2)`, {origin: 'right'})
